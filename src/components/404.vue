9e38717b6718d20bd2cd83449ba99f60fdb824c5
Jack <me@jack003.com>
2017-02-21 11:30:49 +0800

    edit 404 page

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,12 +1,23 @@
 <template>
 <div>
-	404 not found
+	<p>{{ message }}</p>
+	<router-link to="/">Home</router-link>
 </div>
 </template>
 
 <script>
+export default {
+	name: '404',
+	data () {
+		return {
+			message: '404 not found'
+		}
+	}
+}
 </script>
 
 <style scoped>
-
+div {
+	text-align: center;
+}
 </style>
