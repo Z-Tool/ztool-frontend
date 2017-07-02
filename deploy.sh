acf7534ce5829c9d917f377a05c4788eaef9b44f
Jack <me@jarrekk.com>
2017-07-03 01:04:15 +0800

    edit travis deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 rm -rf node_modules
 npm install
 npm run build
-ssh root@vps.jarrekk.com "rm -rf /frontend/*"
-rsync -avz ./dist/* root@vps.jarrekk.com:/frontend/dist/
-ssh root@vps.jarrekk.com "docker restart backhend_nginx_1"
+if [ $? -eq 0 ]; then
+  ssh root@vps.jarrekk.com "rm -rf /frontend/*"
+  rsync -avz ./dist/* root@vps.jarrekk.com:/frontend/dist/
+  ssh root@vps.jarrekk.com "docker restart backhend_nginx_1"
+fi
