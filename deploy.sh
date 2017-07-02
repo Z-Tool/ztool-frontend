1527e1a8f029e5cbf7993197c8e142597e5492b7
Jack <me@jarrekk.com>
2017-07-03 00:52:20 +0800

    edit travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+npm install
+npm run build
 ssh root@vps.jarrekk.com "rm -rf /frontend/*"
 rsync -avz dist root@vps.jarrekk.com:/frontend/
 ssh root@vps.jarrekk.com "docker restart backhend_nginx_1"
